f364fde27704dc74b6a5b7793be666d77b3afee0
imMatheus <matheusmendesbarata03@gmail.com>
2023-03-05 17:26:01 +0100

    made HeaderView into own component

---
+++ src/components/restaurants/HeaderView.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import React from "react";
+import { Star } from "react-feather";
+import type { RouterOutputs } from "~/utils/api";
+
+interface HeaderViewProps {
+  restaurant: NonNullable<RouterOutputs["restaurants"]["getById"]>;
+}
+
+export const HeaderView: React.FC<HeaderViewProps> = ({ restaurant }) => {
+  return (
+    <div className="mb-4 flex flex-col gap-4 sm:flex-col-reverse">
+      <div className="relative grid h-44 overflow-hidden rounded-lg sm:h-72 sm:grid-cols-4 sm:grid-rows-2 sm:gap-2 md:h-96">
+        <div className="relative sm:col-span-2 sm:row-span-2">
+          <Image
+            src={restaurant.images[0] || ""}
+            alt={`${restaurant.name} image`}
+            fill={true}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+
+        {restaurant.images.slice(1, 5).map((image, index) => (
+          <div className="relative hidden sm:block" key={index}>
+            <Image
+              src={image}
+              alt={`${restaurant.name} image`}
+              fill={true}
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="">
+        <h1 className="text-2xl font-semibold tracking-wide">
+          {restaurant.name}
+        </h1>
+        <div className="flex flex-wrap items-center gap-2 text-sm">
+          <div className="flex items-center gap-1">
+            <Star className="h-[14px] w-[14px] fill-text text-text" />
+            <p>{restaurant.rating.toFixed(1)}</p>
+          </div>
+          <div className="-mx-0.5 h-0.5 w-0.5 rounded-full bg-text"></div>
+          <div className="flex items-center gap-1">
+            <p>{restaurant.numberOfReviews} reviews</p>
+          </div>
+          <div className="-mx-0.5 h-0.5 w-0.5 rounded-full bg-text"></div>
+          <div className="flex items-center gap-1">
+            <p>
+              {restaurant.address.street}, {restaurant.address.city}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/restaurants/[id]/index.tsx
+++ src/pages/restaurants/[id]/index.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { Star } from "react-feather";
 import { MenuView } from "~/components/restaurants/MenuView";
 import { ReviewsView } from "~/components/restaurants/ReviewsView";
+import { HeaderView } from "~/components/restaurants/HeaderView";
 
 const Restaurant: NextPage = () => {
   const router = useRouter();
@@ -30,50 +31,7 @@ const Restaurant: NextPage = () => {
   return (
     <div className="px-4 sm:py-5">
       <div className="mx-auto max-w-7xl">
-        <div className="mb-4 flex flex-col gap-4 sm:flex-col-reverse">
-          <div className="relative grid h-44 overflow-hidden rounded-lg sm:h-72 sm:grid-cols-4 sm:grid-rows-2 sm:gap-2 md:h-96">
-            <div className="relative sm:col-span-2 sm:row-span-2">
-              <Image
-                src={restaurant.images[0] || ""}
-                alt={`${restaurant.name} image`}
-                fill={true}
-                style={{ objectFit: "cover" }}
-              />
-            </div>
-
-            {restaurant.images.slice(1, 5).map((image, index) => (
-              <div className="relative hidden sm:block" key={index}>
-                <Image
-                  src={image}
-                  alt={`${restaurant.name} image`}
-                  fill={true}
-                  style={{ objectFit: "cover" }}
-                />
-              </div>
-            ))}
-          </div>
-          <div className="">
-            <h1 className="text-2xl font-semibold tracking-wide">
-              {restaurant.name}
-            </h1>
-            <div className="flex flex-wrap items-center gap-2 text-sm">
-              <div className="flex items-center gap-1">
-                <Star className="h-[14px] w-[14px] fill-text text-text" />
-                <p>{restaurant.rating.toFixed(1)}</p>
-              </div>
-              <div className="-mx-0.5 h-0.5 w-0.5 rounded-full bg-text"></div>
-              <div className="flex items-center gap-1">
-                <p>{restaurant.numberOfReviews} reviews</p>
-              </div>
-              <div className="-mx-0.5 h-0.5 w-0.5 rounded-full bg-text"></div>
-              <div className="flex items-center gap-1">
-                <p>
-                  {restaurant.address.street}, {restaurant.address.city}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <HeaderView restaurant={restaurant} />
 
         {restaurant.menu && <MenuView menu={restaurant.menu} />}
 
